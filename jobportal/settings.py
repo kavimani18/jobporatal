fdd3e875a0610c7e680a51aec86ae9e9ece3ac39
kavimani18 <kavimani1110@gmail.com>
2025-12-18 15:14:30 +0530

    Update ALLOWED_HOSTS for local and Render deployment

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-$bvop5%91nyqan@r+i8_&x5e8@nln)xv8481thu1rhlhtx2c^i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com']
+
 
 
 # Application definition
